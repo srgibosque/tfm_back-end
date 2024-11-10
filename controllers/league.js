1e8224eb1414aa6a3704548edda886a5a493467c
[Sergi Bosque Ródenas] <[sbosquero@uoc.edu]>
2024-11-10 14:25:30 +0100

    leagueController: deleteLeague

--- controllers/league.js
+++ controllers/league.js
@@ -1,5 +1,6 @@
-const User = require('../models/user');
+const Team = require('../models/team');
 const League = require('../models/league');
+const Match = require('../models/match');
 const handleError = require('../util/error-handler');
 const duplicateUniqueValuesError = require('../util/duplicate-unique-values_error-handler');
 
@@ -7,18 +8,35 @@ exports.createLeague = async (req, res, next) => {
 
 };
 
-exports.getTeamByUsername = async (req, res, next) => {
+exports.getTeamByTeamname = async (req, res, next) => {
 
 };
 
 exports.updateLeague = async (req, res, next) => {
-  
+
 };
 
 exports.getLeague = async (req, res, next) => {
-  
+
 };
 
 exports.deleteLeague = async (req, res, next) => {
-  
+  const leagueId = req.params.leagueId;
+  try {
+    const league = await League.findByPk(leagueId);
+
+    if (!league) {
+      const error = new Error('League not found');
+      error.statusCode = 404;
+      throw error;
+    }
+
+    await league.destroy();
+
+    res.status(200).json({
+      message: 'League deleted successfully'
+    });
+  } catch (err) {
+    handleError(err, next);
+  }
 };
--- routes/league.js
+++ routes/league.js
@@ -4,6 +4,10 @@ const leagueController = require('../controllers/league')
 const isAuth = require('../middleware/is-auth');
 const router = express.Router();
 
+router.post('/', isAuth, leagueController.createLeague);
+router.get('/team/:teamName', isAuth, leagueController.getTeamByTeamname);
+router.put('/:leagueId', isAuth, leagueController.updateLeague);
 router.get('/:leagueId', isAuth, leagueController.getLeague);
+router.delete('/:leagueId', isAuth, leagueController.deleteLeague);
 
 module.exports = router;
